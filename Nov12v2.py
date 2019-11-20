83c10c0a997f26db438c57514013e72e201fe1c2
drewtuley <drewtuley@gmail.com>
2019-11-20 07:17:10 +0000

    add a funky filter/lambda for dictionary load

--- Nov12v2.py
+++ Nov12v2.py
@@ -1,5 +1,6 @@
 from itertools import permutations
 
+
 # Using the letters AAEENNPPPPSS complete the grid below. The grid reads the same across as down.
 
 # S...
@@ -8,10 +9,7 @@ from itertools import permutations
 # ...T
 
 
-dictionary = []
-
-
-def test_words_against_dictionary(words, valid_words):
+def test_words_against_dictionary(words, valid_words, dictionary):
     invalid_words = []
     print('Test words: ' + ','.join(words))
     for word_to_test in words:
@@ -26,38 +24,35 @@ def test_words_against_dictionary(words, valid_words):
 
 if __name__ == '__main__':
     with open('20k.txt') as fd:
-        for raw_l in fd:
-            l = raw_l.strip()
-            if len(l) == 4:
-                dictionary.append(l)
+        dictionary = set(filter(lambda word: (len(word) == 4), [x.strip() for x in fd]))
 
-    test = test_words_against_dictionary(['snap', 'nape', 'apes', 'pest'], [])
-    if len(test) == 0:
-        print('test passes OK')
-    else:
-        print('Fail: Those words should pass OK')
-        exit(1)
+        test = test_words_against_dictionary(['snap', 'nape', 'apes', 'pest'], [], dictionary)
+        if len(test) == 0:
+            print('test passes OK')
+        else:
+            print('Fail: Those words should pass OK')
+            exit(1)
 
-    invalid_word_cache = []
-    valid_word_cache = []
-    # as the grid is a diagonally symmetric we only need half the letters
-    for p in permutations(['a', 'e', 'n', 'p', 'p', 's']):
-        # form the 4 words using the following indices into 'p'
-        # S012
-        # 0A34
-        # 13E5
-        # 245T
-        words_to_test = [
-            's' + p[0] + p[1] + p[2],
-            p[0] + 'a' + p[3] + p[4],
-            p[1] + p[3] + 'e' + p[5],
-            p[2] + p[4] + p[5] + 't'
-        ]
+        invalid_word_cache = []
+        valid_word_cache = []
+        # as the grid is a diagonally symmetric we only need half the letters
+        for p in permutations(['a', 'e', 'n', 'p', 'p', 's']):
+            # form the 4 words using the following indices into 'p'
+            # S012
+            # 0A34
+            # 13E5
+            # 245T
+            words_to_test = [
+                's' + p[0] + p[1] + p[2],
+                p[0] + 'a' + p[3] + p[4],
+                p[1] + p[3] + 'e' + p[5],
+                p[2] + p[4] + p[5] + 't'
+            ]
 
-        if not any(word in invalid_word_cache for word in words_to_test):
-            bad_words = test_words_against_dictionary(words_to_test, valid_word_cache)
-            if len(bad_words) == 0:
-                print(words_to_test)
-                break
-            else:
-                invalid_word_cache.extend(bad_words)
+            if not any(word in invalid_word_cache for word in words_to_test):
+                bad_words = test_words_against_dictionary(words_to_test, valid_word_cache, dictionary)
+                if len(bad_words) == 0:
+                    print(words_to_test)
+                    break
+                else:
+                    invalid_word_cache.extend(bad_words)
