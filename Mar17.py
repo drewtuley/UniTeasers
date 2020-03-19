14826e04dc3a80249db1be40b6dbf98b8b0b1f6c
drewtuley <drewtuley@gmail.com>
2020-03-19 07:04:29 +0000

    comment tweaks mostly

--- Mar17.py
+++ Mar17.py
@@ -1,17 +1,29 @@
-from itertools import permutations
 import re
+from itertools import permutations
 
 
-# Fill in the 4x4 grid with the following letters 'DOGDAYS'
-# such that each row, column and 2x2 block has letters in any order
-# that make common words.
+# Fill in the 4x4 grid below with the following letters 'DOGDAYS'
+# such that each row, column and 2x2 (corner) block has letters (in any order)
+# that make common words. Each letter must only be used once.
 
 # ..C.
 # L..K
 # ATE.
 # .SPI
 
+class FourLetterWords():
+    words = list()
+
+    def __init__(self):
+        self.words = list()
+
+    def append(self, word):
+        if len(set([x for x in word])) == 4:
+            self.words.append(word)
+
+
 def get_possible_words():
+    '''Construct possible 'words' by taking each permutation of DOGDAYS and inserting them into the blank space in the grid '''
     possible_words = set()
     for combo in permutations(allowed_letters, 7):
         word = matrix
@@ -22,29 +34,21 @@ def get_possible_words():
 
 
 def get_four_letter_words(possible_word):
-    four_letter_words = []
+    '''Get the letters for possible four letter words from each row, column and corner 2x2 block'''
+    flws = FourLetterWords()
 
-    four_letter_words.append(possible_word[0:4])
-    four_letter_words.append(possible_word[4:8])
-    four_letter_words.append(possible_word[8:12])
-    four_letter_words.append(possible_word[12:])
     for idx in range(0, 4):
+        flws.append(possible_word[idx * 4:(idx * 4) + 4])
         w = possible_word[idx] + possible_word[idx + 4] + possible_word[idx + 8] + possible_word[idx + 12]
-        four_letter_words.append(w)
+        flws.append(w)
 
-    four_letter_words.append(possible_word[0:2] + possible_word[4:6])
-    four_letter_words.append(possible_word[2:4] + possible_word[6:8])
-    four_letter_words.append(possible_word[8:10] + possible_word[12:14])
-    four_letter_words.append(possible_word[10:12] + possible_word[14:])
+    flws.append(possible_word[0:2] + possible_word[4:6])
+    flws.append(possible_word[2:4] + possible_word[6:8])
+    flws.append(possible_word[8:10] + possible_word[12:14])
+    flws.append(possible_word[10:12] + possible_word[14:])
 
-    for word in four_letter_words:
-        letters = set()
-        for letter in word:
-            letters.add(letter)
-        if len(letters) != 4:
-            return []
+    return flws.words
 
-    return four_letter_words
 
 if __name__ == '__main__':
     allowed_letters = 'dogdays'
@@ -52,22 +56,18 @@ if __name__ == '__main__':
 
     with open('20k.txt') as fd:
         dictionary = set(
-            filter(lambda wd: (len(wd) >= 3 and len(wd)< 5), [x.strip() for x in fd]))
+            filter(lambda wd: (3 <= len(wd) < 5), [x.strip() for x in fd]))
 
-    possible_words = get_possible_words()
-    for possible_word in possible_words:
-        four_letter_words = get_four_letter_words(possible_word)
+        for possible_word in get_possible_words():
+            four_letter_words = get_four_letter_words(possible_word)
+            if len(four_letter_words) == 12:
+                good_words = []
+                for word in four_letter_words:
+                    for perm_word in permutations(word, 4):
+                        w = ''.join(perm_word)
+                        if w in dictionary or (w[3] == 's' and w[0:3] in dictionary):
+                            good_words.append(w)
+                            break
 
-        good_words = []
-        for word in four_letter_words:
-            for perm_word in permutations(word, 4):
-                w = ''.join(perm_word)
-                if w in dictionary:
-                    good_words.append(w)
-                    break
-                if w[3] == 's' and w[0:3] in dictionary:
-                    good_words.append(w)
-                    break
-
-        if len(good_words) == 12:
-            print(possible_word, good_words)
+                if len(good_words) == 12:
+                    print(possible_word, good_words)
